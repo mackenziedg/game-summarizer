3cf7ed3ad4d23065e82c174459ba63761c6203ec
Mackenzie Gray <me@mackenziedgray.com>
2024-05-09 17:51:02 -0400

    Use amplify adapter.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
-import Carousel from "svelte-carousel";
-import GameSummary from "./GameSummary.svelte";
-import type { PageServerData } from "./$types";
+  import Carousel from "svelte-carousel";
+  import GameSummary from "./GameSummary.svelte";
+  import type { PageServerData } from "./$types";
 
-export let data: PageServerData;
+  export let data: PageServerData;
 
-let date = new Date();
-date.setDate(date.getDate() - 1);
-const yesterday = date.toLocaleString().split(", ")[0];
+  let date = new Date();
+  date.setDate(date.getDate() - 1);
+  const yesterday = date.toLocaleString().split(", ")[0];
 </script>
 
 <div class="flex-col p-10">
   <header>
     <a href="/about" class="float-right clear-both hover:text-orange-300"
-    >About</a
+      >About</a
     >
     <h1 class="text-center text-3xl font-bold">
       Game Summaries for {yesterday}
@@ -21,20 +21,14 @@ const yesterday = date.toLocaleString().split(", ")[0];
   </header>
 
   <div class="mt-4">
-
-    {#await data}
-      {:then d}
+    {#await data then d}
       <Carousel>
-
         {#each d.summaries as summary}
-          {#await summary}
-            {:then s}
+          {#await summary then s}
             <GameSummary summary={s} />
           {/await}
         {/each}
-
       </Carousel>
     {/await}
-
   </div>
 </div>
