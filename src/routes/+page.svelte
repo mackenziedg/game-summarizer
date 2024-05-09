ae2503c1fc8ad06895840ab22ab5a571e303c678
Mackenzie Gray <me@mackenziedgray.com>
2024-05-09 17:41:48 -0400

    Use svelte-carousel.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,40 @@
 <script lang="ts">
-  import GameSummary from "./GameSummary.svelte";
-  import type { PageServerData } from "./$types";
+import Carousel from "svelte-carousel";
+import GameSummary from "./GameSummary.svelte";
+import type { PageServerData } from "./$types";
 
-  export let data: PageServerData;
+export let data: PageServerData;
 
-  let date = new Date();
-  date.setDate(date.getDate() - 1);
-  const yesterday = date.toLocaleString().split(", ")[0];
+let date = new Date();
+date.setDate(date.getDate() - 1);
+const yesterday = date.toLocaleString().split(", ")[0];
 </script>
 
 <div class="flex-col p-10">
   <header>
     <a href="/about" class="float-right clear-both hover:text-orange-300"
-      >About</a
+    >About</a
     >
     <h1 class="text-center text-3xl font-bold">
       Game Summaries for {yesterday}
     </h1>
   </header>
 
-  {#await data then d}
-    <div class="flex overflow-x-auto justify-left mt-8">
-      {#each d.summaries as summary}
-        {#await summary then s}
-          <GameSummary summary={s} />
-        {/await}
-      {/each}
-    </div>
-  {/await}
+  <div class="flex mt-4">
+
+    {#await data}
+      {:then d}
+      <Carousel>
+
+        {#each d.summaries as summary}
+          {#await summary}
+            {:then s}
+            <GameSummary summary={s} />
+          {/await}
+        {/each}
+
+      </Carousel>
+    {/await}
+
+  </div>
 </div>
--- src/routes/GameSummary.svelte
+++ src/routes/GameSummary.svelte
@@ -7,10 +7,7 @@
 
 <div
   class="
-  self-start
-  min-w-fit
   pt-4 pb-4
-  ml-10 mr-10
   rounded-lg
   bg-zinc-200
   dark:bg-zinc-700"
