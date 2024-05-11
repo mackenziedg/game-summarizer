0f2082a997405e6db246c0e72ae5a518ce3eb1da
Mackenzie Gray <me@mackenziedgray.com>
2024-05-11 16:13:36 -0400

    Allow translation.

--- src/routes/GameSummaryBody.svelte
+++ src/routes/GameSummaryBody.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
   export let summary;
+
+  $: langId = "summary_en";
+  $: summaryText = summary[langId]
+  const switchLang = () => {
+    if (langId === "summary_en") {
+      langId = "summary_es";
+    } else {
+      langId = "summary_en";
+    }
+  };
 </script>
 
 <div class="px-4 lg:px-8 mt-4 text-sm lg:text-base xl:text-lg inconsolata">
+  <button on:click={switchLang} class="font-bold hover:text-orange-300">{langId.split("_")[1]}</button>
   <p>
-    {@html summary.summary.replace("\n", "<br/><br/>")}
+    {@html summaryText.replace("\n", "<br/><br/>")}
   </p>
 </div>
