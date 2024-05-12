ecb9181102a06831cb2b800efcfd40773ff9f86c
Mackenzie Gray <me@mackenziedgray.com>
2024-05-12 16:01:36 -0400

    Use stores to globally set language choice.

--- src/routes/GameSummaryBody.svelte
+++ src/routes/GameSummaryBody.svelte
@@ -1,20 +1,30 @@
 <script lang="ts">
+  import { lang } from "./stores.ts";
+
   export let summary;
 
-  $: langId = "summary_en";
-  $: summaryText = summary[langId]
-  const switchLang = () => {
-    if (langId === "summary_en") {
-      langId = "summary_es";
-    } else {
-      langId = "summary_en";
-    }
+  let langValue;
+
+  lang.subscribe((v: string) => {
+    langValue = `summary_${v}`;
+  });
+
+  const switchLang: string = () => {
+    lang.update((v: string) => {
+      if ($lang === "en") {
+        return "es";
+      } else if ($lang == "es") {
+        return "en";
+      }
+    });
   };
 </script>
 
 <div class="px-4 lg:px-8 mt-4 text-sm lg:text-base xl:text-lg inconsolata">
-  <button on:click={switchLang} class="font-bold hover:text-orange-300">{langId.split("_")[1]}</button>
+  <button on:click={switchLang} class="font-bold hover:text-orange-300"
+    >{$lang}</button
+  >
   <p>
-    {@html summaryText.replace("\n", "<br/><br/>")}
+    {@html summary[langValue].replace("\n", "<br/><br/>")}
   </p>
 </div>
